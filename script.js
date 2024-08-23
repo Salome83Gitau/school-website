08887204907bf4ec8c8cbf97f53755ceca960fb8
Salome83Gitau <salome83wambui@gmail.com>
2024-08-23 20:45:50 +0300

    navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
